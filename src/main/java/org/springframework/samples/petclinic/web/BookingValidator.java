3c3caef617ada22ee9e0baa86deb250dd50c2675
alvesccas221 <alvesccas@alum.us.es>
2021-04-22 13:26:52 +0200

    Validación formulario booking

--- src/main/java/org/springframework/samples/petclinic/web/BookingValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/BookingValidator.java
@@ -14,38 +14,28 @@ import org.springframework.validation.Validator;
 public class BookingValidator implements Validator {
 	private static final String REQUIRED = "required";
 
-	
 	@Override
 	public void validate(Object obj, Errors errors) {
-//		Pet pet = (Pet) obj;
-//		String name = pet.getName();
 		Booking booking = (Booking) obj;
-	if(booking.getFechaEntrada()==null||booking.getFechaSalida()==null) {
-		errors.rejectValue("fechaEntrada", REQUIRED, REQUIRED);
-	}else {
-	if(booking.getFechaEntrada().before(Date.valueOf(LocalDate.now()))) {
-		errors.rejectValue("fechaEntrada", "error","La fecha de entrada debe ser posterior o igual al día de hoy");
-	}
-	
-	if(booking.getFechaSalida().before(Date.valueOf(LocalDate.now()))) {
-		errors.rejectValue("fechaSalida", "error","La fecha de salida debe ser posterior o igual al día de hoy");
-	}
-	
-	if(booking.getFechaSalida()==null ||booking.getFechaSalida().before(booking.getFechaEntrada())) {
-		errors.rejectValue("fechaSalida", "error","La fecha de salida debe ser posterior o igual a la fecha de entrada");
-	}
-	
+		if (booking.getFechaEntrada() == null || booking.getFechaSalida() == null) {
+			errors.rejectValue("fechaEntrada", REQUIRED, REQUIRED);
+		} else {
+			if (booking.getFechaEntrada().before(Date.valueOf(LocalDate.now()))) {
+				errors.rejectValue("fechaEntrada", "error",
+						"La fecha de entrada debe ser posterior o igual al día de hoy");
+			}
 
-	}
-//		// type validation
-//		if (booking.getFechaEntrada().after(booking.getFechaSalida())) {
-//			errors.rejectValue("date", REQUIRED, REQUIRED);
-//		}
-//
-//		// birth date validation
-//		if (booking.getFechaEntrada()==null || booking.getFechaSalida()==null) {
-//			errors.rejectValue("date", REQUIRED, REQUIRED);
-//		}
+			if (booking.getFechaSalida().before(Date.valueOf(LocalDate.now()))) {
+				errors.rejectValue("fechaSalida", "error",
+						"La fecha de salida debe ser posterior o igual al día de hoy");
+			}
+
+			if (booking.getFechaSalida() == null || booking.getFechaSalida().before(booking.getFechaEntrada())) {
+				errors.rejectValue("fechaSalida", "error",
+						"La fecha de salida debe ser posterior o igual a la fecha de entrada");
+			}
+
+		}
 	}
 
 	/**
